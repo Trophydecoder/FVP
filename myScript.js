6adb9ce3f2e3d1db94acd8c610cb3fd83818166e
Trophydecoder <trophydecoder@gmail.com>
2024-08-15 23:31:05 +0200

    first commit

---
+++ myScript.js
@@ -0,0 +1,31 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  
+  
+
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+
+  document.getElementById('mar')
+              .addEventListener('click', function () {
+                window.location.href = 'file:///C:/Users/Sthe/Documents/websites/Flowing%20Visuals/Marketing%20intelligence.html';
+              });
+  document.getElementById('exe')
+              .addEventListener('click', function () {
+                window.location.href = 'file:///C:/Users/Sthe/Documents/websites/Flowing%20Visuals/Execution%20Intelligency.html';
+              });           
+  document.getElementById('btn')
+              .addEventListener('click', function () {
+                window.location.href = 'file:///C:/Users/Sthe/Documents/websites/Flowing%20Visuals/contactus.html';
+              });           
